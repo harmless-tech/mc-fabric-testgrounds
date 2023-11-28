448de81e1bdf33907e6ffb12139de355aa6af191
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-27 23:20:23 -0500

    spotless/spotbugs

--- src/client/java/tech/harmless/mc/fabric/testgrounds/TestgroundsClient.java
+++ src/client/java/tech/harmless/mc/fabric/testgrounds/TestgroundsClient.java
@@ -3,8 +3,8 @@ package tech.harmless.mc.fabric.testgrounds;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TestgroundsClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
