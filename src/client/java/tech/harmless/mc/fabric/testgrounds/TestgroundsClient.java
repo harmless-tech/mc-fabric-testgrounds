07ed6d3cc72df6121abc0f42440cb524b9213dc5
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-27 22:51:13 -0500

    Init

---
+++ src/client/java/tech/harmless/mc/fabric/testgrounds/TestgroundsClient.java
@@ -0,0 +1,10 @@
+package tech.harmless.mc.fabric.testgrounds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestgroundsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
